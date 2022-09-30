9588d2d3bfdb50a1df76c7163a8fce0d7727354e
vicente <vostornol@gmail.com>
2022-09-29 22:28:01 -0300

    Add templates and computer management table

--- cyber_system/cyberXapp/urls.py
+++ cyber_system/cyberXapp/urls.py
@@ -4,8 +4,14 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('index_in/', views.index_in, name='index_in'),
     # path('create/customer/', views.create_customer(), name='costumer'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('login/', views.login, name='login'),
+    path('computer_management/', views.computer_management, name='computer_management'),
+    path('add_edit_computer/', views.add_editComputer, name='add_editComputer'),
+    path('computer_detail/<int:pk>/', views.computer_detail, name='computer_detail'),
+    path('computer_edit/<int:pk>/', views.computer_edit, name='computer_edit'),
+    path('computer_delete/<int:pk>/', views.computer_delete, name='computer_delete'),
 ]
--- cyber_system/cyberXapp/views.py
+++ cyber_system/cyberXapp/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 from . import models
-from . import forms
+from .forms import ComputerForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 # Create your views here.
 
 
@@ -23,4 +24,48 @@ def login(request):
     return render(request, 'state_logged_out/login.html', {
     })
 
+def index_in(request):
+    return render(request, 'state_logged_in/index_in.html', {
+    })
 
+
+def computer_management(request):
+    computers = models.Computer.objects.all()
+
+    return render(request, 'state_logged_in/computers_detail.html',
+    {'computers': computers})
+
+def computer_detail(request, pk):
+    computer = models.Computer.objects.get(id=pk)
+    return render(request, 'state_logged_in/computer_detail.html', 
+    {'computer': computer})
+
+
+def computer_edit(request, pk):
+    computer = models.Computer.objects.get(id=pk)
+    form = ComputerForm(instance=computer)
+    if request.method == 'POST':
+        form = ComputerForm(request.POST, instance=computer)
+        if form.is_valid():
+            form.save()
+            return redirect('computer_management')
+    return render(request, 'state_logged_in/add_editComputer.html', 
+    {'form_':form})
+
+def computer_delete(request, pk):
+    computer = models.Computer.objects.get(id=pk)
+    if request.method == 'POST':
+        computer.delete()
+        return redirect('computer_management')
+    return render(request, 'state_logged_in/computer_delete.html', 
+    {'computer': computer})
+
+def add_editComputer(request):
+    form = ComputerForm()
+    if request.method == 'POST':
+        form = ComputerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('computer_management')
+    return render(request, 'state_logged_in/add_editComputer.html', 
+    {'form_':form})
